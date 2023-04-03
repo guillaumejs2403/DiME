ee79b45409b0b410ce5bc8be9af5fc0a0a6a1af9
Guillaume Jeanneret <guillaume.jeanneret-sanmiguel@unicaen.fr>
2023-04-03 09:58:51 +0200

    typo

--- celeba-train-diffusion.py
+++ celeba-train-diffusion.py
@@ -4,16 +4,16 @@ Train a diffusion model on images.
 
 import argparse
 
-from guided_diffusion import dist_util, logger
-from guided_diffusion.image_datasets import load_data_celeba
-from guided_diffusion.resample import create_named_schedule_sampler
-from guided_diffusion.script_util import (
+from core import dist_util, logger
+from core.image_datasets import load_data_celeba
+from core.resample import create_named_schedule_sampler
+from core.script_util import (
     model_and_diffusion_defaults,
     create_model_and_diffusion,
     args_to_dict,
     add_dict_to_argparser,
 )
-from guided_diffusion.train_util import TrainLoop
+from core.train_util import TrainLoop
 
 
 def main():
